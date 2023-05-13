58fbc04ed8141fcc8530d4fc0ba525e8f0173f6b
jonei Araujo <jonei.araujo@hotmail.com>
2023-05-13 01:02:10 -0300

    SellersController

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using Microsoft.AspNetCore.Connections.Features;
-using SalesWebMvc.Data;
+
 
 namespace SalesWebMvc
 {
