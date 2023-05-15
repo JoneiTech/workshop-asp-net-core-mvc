6c18a7c4f2d65ba8b28fc8eff6382d8d6ce8355d
jonei Araujo <jonei.araujo@hotmail.com>
2023-05-15 19:56:50 -0300

    Foreign key not null (referencial integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -15,6 +15,8 @@ namespace SalesWebMvc.Models
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+        public int DepartmentId { get; set; }
+
         public Seller() { }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
